7273615220025b5c233204ca0884ecec28b414f8
Trung Ho Ngoc - macpro 13 2018 <trung0194@gmai.com>
2020-12-11 11:17:03 +0700

    Create some configs file

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+// The values provided below are the defaults.
+// If you don't specify one of these properties,
+// the default value will be applied.
+module.exports = {
+  printWidth: 140,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none', // other options `none` or `es5` or `all`
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  arrowParens: 'avoid', // other option 'always'
+  quoteProps: 'consistent'
+}
